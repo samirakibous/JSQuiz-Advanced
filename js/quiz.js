103825a101aa17b9510e0e8ff5bcd6c80756b0b0
samira kibous <kiboussamira71@gmail.com>
2025-09-27 14:10:24 +0100

    nextQuestion function refactoring

--- js/quiz.js
+++ js/quiz.js
@@ -136,120 +136,127 @@ export function nextQuestion() {
         return;
     }
     index++;
-    // reponseValidee = false;
-    let tempsFinQuestion = Date.now();
-    let tempsPasse = Math.floor((tempsFinQuestion - tempsDebutQuestion) / 1000);
-    tempsParQuestion.push(tempsPasse);
+    reponseValidee = false;
     if (index < questionsFilter.length) {
         afficherQuestion(questionsFilter, index);
     } else {
-        quiz.style.display = "none";
-        controlButtons.style.display = "none";
-        timerDisplay.style.display = "none";
-        let score = 0;
-        // let totaltime=0;
-        for (let i = 0; i < questionsFilter.length; i++) {
-            if (arraysEqual(questionsFilter[i].correcte, userAnswers[i])) {
-                score++;
-            }
-        }
-        const result = {
-            pseudo: localStorage.getItem("pseudoname"),
-            theme: questionsFilter[0]?.theme || "unknown",
-            score: score,
-            Responses: userAnswers,
-            date: new Date().toLocaleString()
-        };
-
-        let results = JSON.parse(localStorage.getItem("results")) || [];
-        results.push(result);
-        localStorage.setItem("results", JSON.stringify(results));
-
-        let resultat = document.querySelector(".resultat");
-        resultat.innerHTML = "";
-        resultat.style.display = "block";
-        resultat.style.padding = "20px";
-        resultat.style.backgroundColor = "#f7f7f7";
-        resultat.style.borderRadius = "10px";
-        resultat.style.maxWidth = "600px";
-        resultat.style.boxShadow = "0 4px 8px rgba(0,0,0,0.1)";
-        resultat.style.fontFamily = "Arial, sans-serif";
-
-        for (let i = 0; i < questionsFilter.length; i++) {
-            const yourAnswer = (userAnswers[i] && userAnswers[i].length > 0)
-                ? userAnswers[i].map(idx => escapeHtml(questionsFilter[i].reponses[idx])).join(", ")
-                : "Aucune réponse";
-            // const correcteAnswer = questionsFilter[i].reponses[questionsFilter[i].correcte];
-            const correcteAnswer = questionsFilter[i].correcte.map(index => escapeHtml(questionsFilter[i].reponses[index])).join(", ");
-            const questionDiv = document.createElement("div");
-            questionDiv.style.marginBottom = "15px";
-            questionDiv.style.padding = "10px";
-            // questionDiv.style.borderLeft = "4px solid #4CAF50";
-            if (arraysEqual(questionsFilter[i].correcte, userAnswers[i])) {
-                questionDiv.style.borderLeft = "4px solid green";
-            } else {
-                questionDiv.style.borderLeft = "4px solid red";
-            }
-            questionDiv.style.backgroundColor = "#fff";
-            questionDiv.style.borderRadius = "5px";
-
-            questionDiv.innerHTML = `
-        <p style="color:black;"><strong style="color:black;">Q${i + 1}:</strong> ${questionsFilter[i].question}</p>
-        <p style="color:black;">Votre réponse: <span style="color:${yourAnswer === correcteAnswer ? "green" : "red"}">${yourAnswer}</span></p>
-        <p style="color:black;">Réponse correcte: ${correcteAnswer}</p>
-    `;
-
-            resultat.appendChild(questionDiv);
-        }
-
-        const scoreDiv = document.createElement("div");
-        scoreDiv.style.textAlign = "center";
-        scoreDiv.style.marginTop = "20px";
-        scoreDiv.style.fontSize = "18px";
-        scoreDiv.style.fontWeight = "bold";
-        scoreDiv.textContent = `Score: ${score} / ${questionsFilter.length}`;
-        resultat.appendChild(scoreDiv);
-        const tempsTotalSec = tempsParQuestion.reduce((acc, t) => acc + t, 0);
-
-        const minutes = Math.floor(tempsTotalSec / 60);
-        const secondes = tempsTotalSec % 60;
-        const tempsTotalFormate = `${minutes}m ${secondes}s`;
-        const tempsDiv = document.createElement("div");
-        tempsDiv.style.textAlign = "center";
-        tempsDiv.style.marginTop = "10px";
-        tempsDiv.style.fontSize = "16px";
-        tempsDiv.style.fontWeight = "normal";
-        tempsDiv.textContent = `Temps total du quiz : ${tempsTotalFormate}`;
-
-        resultat.appendChild(tempsDiv);
-
-        const retourBtn = document.createElement("button");
-        retourBtn.textContent = "Retour à l'accueil";
-        retourBtn.style.display = "block";
-        retourBtn.style.margin = "20px auto";
-        retourBtn.addEventListener("click", () => {
-            questionCounter.style.display = "none";
-            acceuil.style.display = "flex";
-            acceuil.style.flexDirection = "column";
-            acceuil.style.justifyContent = "center";
-            acceuil.style.alignItems = "center";
-            resultat.style.display = "none";
-            // acceuil.style.display = "block";
-            quitterDiv.style.display = "none";
-            timerDisplay.style.display = "none";
-        });
-        const exportpdf = document.createElement("button");
-        exportpdf.textContent = "générer pdf";
-        exportpdf.style.display = "block";
-
-        exportpdf.addEventListener("click", () => {
-            genererPdf(result);
-        });
-
-        resultat.appendChild(retourBtn);
-        resultat.appendChild(exportpdf);
-        // acceuil.style.display = "block";
+        afficherResultats();
     }
+}
+
+function calculerResultats() {
+    let score = 0;
+    for (let i = 0; i < questionsFilter.length; i++) {
+        if (arraysEqual(questionsFilter[i].correcte, userAnswers[i])) {
+            score++;
+        }
+    }
+
+    const result = {
+        pseudo: localStorage.getItem("pseudoname"),
+        theme: questionsFilter[0]?.theme || "unknown",
+        score: score,
+        Responses: userAnswers,
+        date: new Date().toLocaleString()
+    };
+
+    let results = JSON.parse(localStorage.getItem("results")) || [];
+    results.push(result);
+    localStorage.setItem("results", JSON.stringify(results));
+
+    return result;
+}
+
+function afficherResultats() {
+    quiz.style.display = "none";
+    controlButtons.style.display = "none";
+    timerDisplay.style.display = "none";
+
+    const result = calculerResultats();
+
+    let resultat = document.querySelector(".resultat");
+    resultat.innerHTML = "";
+    resultat.style.display = "block";
+    resultat.style.padding = "20px";
+    resultat.style.backgroundColor = "#f7f7f7";
+    resultat.style.borderRadius = "10px";
+    resultat.style.maxWidth = "600px";
+    resultat.style.boxShadow = "0 4px 8px rgba(0,0,0,0.1)";
+    resultat.style.fontFamily = "Arial, sans-serif";
+
+    questionsFilter.forEach((q, i) => {
+        const yourAnswer = (userAnswers[i] && userAnswers[i].length > 0)
+            ? userAnswers[i].map(idx => escapeHtml(q.reponses[idx])).join(", ")
+            : "Aucune réponse";
+
+        const correcteAnswer = q.correcte.map(index => escapeHtml(q.reponses[index])).join(", ");
+
+        const questionDiv = document.createElement("div");
+        questionDiv.style.marginBottom = "15px";
+        questionDiv.style.padding = "10px";
+        questionDiv.style.borderLeft = arraysEqual(q.correcte, userAnswers[i]) ? "4px solid green" : "4px solid red";
+        questionDiv.style.backgroundColor = "#fff";
+        questionDiv.style.borderRadius = "5px";
+        console.log(q.question);
+        questionDiv.innerHTML = `
+            <p style="color:black;"><strong>Q${i + 1}:</strong> ${q.question}</p>
+            <p style="color:black;">Votre réponse: <span style="color:${yourAnswer === correcteAnswer ? "green" : "red"}">${yourAnswer}</span></p>
+            <p style="color:black;">Réponse correcte: ${correcteAnswer}</p>
+        `;
+
+        resultat.appendChild(questionDiv);
+    });
+
+    const scoreDiv = document.createElement("div");
+    scoreDiv.style.textAlign = "center";
+    scoreDiv.style.marginTop = "20px";
+    scoreDiv.style.fontSize = "18px";
+    scoreDiv.style.fontWeight = "bold";
+    scoreDiv.textContent = `Score: ${result.score} / ${questionsFilter.length}`;
+    resultat.appendChild(scoreDiv);
+
+    afficherTempsTotal(resultat);
+    ajouterBoutons(resultat, result);
+}
+function ajouterBoutons(container, result) {
+    const retourBtn = document.createElement("button");
+    retourBtn.textContent = "Retour à l'accueil";
+    retourBtn.style.display = "block";
+    retourBtn.style.margin = "20px auto";
+    retourBtn.addEventListener("click", () => {
+        questionCounter.style.display = "none";
+        acceuil.style.display = "flex";
+        acceuil.style.flexDirection = "column";
+        acceuil.style.justifyContent = "center";
+        acceuil.style.alignItems = "center";
+        container.style.display = "none";
+        quitterDiv.style.display = "none";
+        timerDisplay.style.display = "none";
+    });
+
+    const exportpdf = document.createElement("button");
+    exportpdf.textContent = "Générer PDF";
+    exportpdf.style.display = "block";
+    exportpdf.addEventListener("click", () => genererPdf(result));
+
+    container.appendChild(retourBtn);
+    container.appendChild(exportpdf);
+}
+
+function afficherTempsTotal(container) {
+    const tempsTotalSec = tempsParQuestion.reduce((acc, t) => acc + t, 0);
+    const minutes = Math.floor(tempsTotalSec / 60);
+    const secondes = tempsTotalSec % 60;
+    const tempsTotalFormate = `${minutes}m ${secondes}s`;
+
+    const tempsDiv = document.createElement("div");
+    tempsDiv.style.textAlign = "center";
+    tempsDiv.style.marginTop = "10px";
+    tempsDiv.style.fontSize = "16px";
+    tempsDiv.style.fontWeight = "normal";
+    tempsDiv.textContent = `Temps total du quiz : ${tempsTotalFormate}`;
+
+    container.appendChild(tempsDiv);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
